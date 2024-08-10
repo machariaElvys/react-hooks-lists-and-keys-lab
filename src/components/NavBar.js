d8ab2de22ca5ff2c6c18e568818fe7aadc5bf3ff
Macharia <machariaelvys@gmail.com>
2024-08-10 08:55:58 +0300

    key lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,17 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>
+    {
+      <div>
+       <a href="#home">home</a>
+       <a href="#about">about</a>
+       <a href="#projects">projects</a>
+      </div>
+
+    
+    }
+    </nav>;
 }
 
 export default NavBar;
